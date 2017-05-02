d55976431b5fa01d9de205a3ed87aeb62dca726c
cb <cb@gmail.com>
2017-05-02 12:51:30 +0200

    Form with createView ok

--- music/urls.py
+++ music/urls.py
@@ -6,6 +6,9 @@ app_name = 'music'
 urlpatterns = [
     url(r'^$',views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = 'detail'),
+    #pas de pk puisque add...
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
 ]
 
 '''
